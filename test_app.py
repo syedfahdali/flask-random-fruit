13da14c9303ecdcf236f2a9f6eeeb8f687b6a4ce
Syed Fahd Ali <100057960+syedfahdali@users.noreply.github.com>
2025-03-16 02:28:33 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,10 @@
-from app import random_fruit
+import sys
+import os
 
+# Add the directory containing `app` to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import random_fruit
 
 def test_random_fruit():
     assert "apple" or "cherry" or "orange" in random_fruit()
